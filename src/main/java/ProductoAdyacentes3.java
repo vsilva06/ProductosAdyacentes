8952add461e5512113711b033018911fc99c491c
Víctor Mardones <v.mardones04@ufromail.cl>
2020-05-28 16:38:19 -0400

    Metodo ProductoAdyacente3Test

--- src/main/java/ProductoAdyacentes3.java
+++ src/main/java/ProductoAdyacentes3.java
@@ -1,9 +1,17 @@
-public class ProductoAdyacente3 {
+public class ProductoAdyacentes3 {
 
     public int productoAdyacentes3(int[] arr){
         int mayor = 0;
 
-        if(arr!=null && arr.length>=2) { // Arreglo no nulo y debe tener al menos 2 numeros
+        if(arr==null){
+            System.out.println("Error: Arreglo vacío");
+        }
+
+        else if(arr.length<2){
+            System.out.println("Error: Arreglo demasiado corto");
+        }
+
+        else{ // Arreglo no nulo y debe tener al menos 2 numeros
             mayor = arr[0]*arr[1];
             for (int i=1; i<arr.length-1; i++) {
                 if (arr[i]*arr[i+1] > mayor) {
