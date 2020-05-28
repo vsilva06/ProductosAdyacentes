3f734e9b0158d1df1ca35ca545cd4983e73cd7b0
Víctor Mardones <v.mardones04@ufromail.cl>
2020-05-28 16:09:55 -0400

    Metodo ProductoAdyacente3

--- src/main/java/ProductoAdyacente3.java
+++ src/main/java/ProductoAdyacente3.java
@@ -1,2 +1,17 @@
 public class ProductoAdyacente3 {
+
+    public static int productoAdyacentes3(int[] arr){
+        int mayor = 0;
+
+        if(arr.length>=2) {
+            mayor = arr[0]*arr[1];
+            for (int i=1; i<arr.length-1; i++) {
+                if (arr[i]*arr[i+1] > mayor) {
+                    mayor = arr[i] * arr[i+1];
+                }
+            }
+        }
+        return mayor;
+    }
+
 }
