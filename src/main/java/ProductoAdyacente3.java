24591a4ba65d2e6fbca14c6f184ee9d074e033fd
Víctor Mardones <v.mardones04@ufromail.cl>
2020-05-28 16:20:24 -0400

    Metodo ProductoAdyacente3Test

--- src/main/java/ProductoAdyacente3.java
+++ src/main/java/ProductoAdyacente3.java
@@ -1,9 +1,9 @@
 public class ProductoAdyacente3 {
 
-    public static int productoAdyacentes3(int[] arr){
+    public int productoAdyacentes3(int[] arr){
         int mayor = 0;
 
-        if(arr.length>=2) {
+        if(arr!=null && arr.length>=2) { // Arreglo no nulo y debe tener al menos 2 numeros
             mayor = arr[0]*arr[1];
             for (int i=1; i<arr.length-1; i++) {
                 if (arr[i]*arr[i+1] > mayor) {
