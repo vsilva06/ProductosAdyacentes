d80cb9298297ac4398d9ef94f4ddf80659a9dcae
Vicente Silva <v.silva06@ufromail.cl>
2020-05-28 16:40:14 -0400

    Clase productoadyacentes 2
    vicho

---
+++ src/test/java/ProductoAdyacentes2Test.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductoAdyacentes2Test {
+    private ProductoAdyacentes2 c;
+
+    @Before
+    public void setUp() throws Exception {
+        c = new ProductoAdyacentes2();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        c = null;
+    }
+
+    @Test
+    public void productoAdyacente() {
+        String num0[] = {"1", "2", "3"};
+        double num1[] = {1, 2.2, 5, 0.32};
+        System.out.println(c.productoAdyacente(num0));
+        System.out.println(c.productoAdyacente(num1));
+
+    }
+}
