896a8cef83d3c70622e8bf0aa822c419586efe93
Vicente Silva <v.silva06@ufromail.cl>
2020-05-31 16:52:16 -0400

    fix

--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 
 public class ProductoAdyacentesTest {
     private ProductoAdyacentes p;
+
     @Before
     public void setUp() throws Exception {
         p = new ProductoAdyacentes();
@@ -20,23 +21,22 @@ public class ProductoAdyacentesTest {
     @Test
     public void productoAdyacentes() {
         int[] arreglo = {};
-        assertEquals(p.productoAdyacentes(arreglo),0);
+        assertEquals(p.productoAdyacentes(arreglo), 0);
         int[] arreglo0 = {0};
-        assertEquals(p.productoAdyacentes(arreglo0),0);
+        assertEquals(p.productoAdyacentes(arreglo0), 0);
         int[] arreglo1 = {1, -4, 2, 2, 5, -1};
-        assertEquals(p.productoAdyacentes(arreglo1),10);
+        assertEquals(p.productoAdyacentes(arreglo1), 10);
         int[] arreglo2 = {1, 2, -12, 10, -10, -12};
-        assertEquals(p.productoAdyacentes(arreglo2),120);
-        int[] arreglo3 = {500, -500, -200, 4, 50, 1000 ,12 ,-12};
-        assertEquals(p.productoAdyacentes(arreglo3),100000);
+        assertEquals(p.productoAdyacentes(arreglo2), 120);
+        int[] arreglo3 = {500, -500, -200, 4, 50, 1000, 12, -12};
+        assertEquals(p.productoAdyacentes(arreglo3), 100000);
         int[] arr2 = {1, -1, 1, -1, 1, -1};
         assertEquals(-1, p.productoAdyacentes(arr2));
         int[] arr1 = {1, -4, 2, 2, 5, -1};
-        int a =p.productoAdyacentes(arr1);
-        assertEquals(10, a);
+        int[] a = {1, 2, 69, 8, 6};
+        assertEquals(10, p.productoAdyacentes(arr1));
         double[] num0 = {1073741823, 1073741823, 1073741545};
         System.out.println(p.productoAdyacentes(num0));
-
 
 
     }
