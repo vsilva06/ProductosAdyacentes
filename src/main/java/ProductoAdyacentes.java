dfb3d3718b0ddd7a1ce7b9e16ce0c7ad944f775e
Felipe Sanzana <f.sanzana01@ufromail.cl>
2020-06-02 22:45:42 -0400

    Mejora metodo y fix tests
    -Metodo mejorado para hacer operaciones con BigInteger.
    test reordenados

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -1,36 +1,37 @@
-import javax.management.StringValueExp;
+import java.math.BigInteger;
 
 class ProductoAdyacentes {
-
-
     long productoAdyacentes(int[] arr) {
-        long mayor = 0;
+        BigInteger mayor = new BigInteger("0");
         if (!validacion(arr)) { // validacion del arreglo
             return 0;
         }
-        mayor = arr[0] * arr[1];
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i] * arr[i + 1] > mayor) {
-                mayor = arr[i] * arr[i + 1];
+        if(arr.length==1){ //
+            return arr[0];
+        }else {
+            BigInteger b1 = BigInteger.valueOf(arr[0]);
+            BigInteger b2 = BigInteger.valueOf(arr[1]);
+            mayor= b1.multiply(b2);
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (arr[i] * arr[i + 1] > mayor.longValue()) {
+                    b1 = BigInteger.valueOf(arr[i]);
+                    b2 = BigInteger.valueOf(arr[i+1]);
+                    mayor= b1.multiply(b2);
+                }
             }
         }
-        return mayor;
+        return mayor.longValue();
     }
 
     private boolean validacion(int[] arr) {
-
-        if (arr == null) {//arreglo vacion retorna error
+        if (arr == null) {//arreglo vacio retorna error
             System.out.println("Error, arreglo vacio");
             return false;
-        } else if (arr.length < 2) {// arreglo muy corto retorna error
+        } else if (arr.length < 1) {// arreglo muy corto retorna error
             System.out.println("Error, arreglo muy corto");
             return false;
         } else {
             return true;
         }
     }
-
-
-
-
 }
--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -20,23 +20,24 @@ public class ProductoAdyacentesTest {
 
     @Test
     public void productoAdyacentes() {
-        int[] arreglo = {};
-        assertEquals(p.productoAdyacentes(arreglo), 0);
-        int[] arreglo0 = {0};
-        assertEquals(p.productoAdyacentes(arreglo0), 0);
+        int[] arreglonull = null;
+        p.productoAdyacentes(arreglonull);
+        int[] arreglovoid = {};
+        p.productoAdyacentes(arreglovoid);
+        int[] arreglo0 = {1};
+        assertEquals(p.productoAdyacentes(arreglo0), 1);
         int[] arreglo1 = {1, -4, 2, 2, 5, -1};
         assertEquals(p.productoAdyacentes(arreglo1), 10);
         int[] arreglo2 = {1, 2, -12, 10, -10, -12};
         assertEquals(p.productoAdyacentes(arreglo2), 120);
-        int[] arreglo3 = {500, -500, -200, 4, 50, 1000, 12, -12};
-        int[] arreglo4 = null;
-        p.productoAdyacentes(arreglo4);
-        assertEquals(p.productoAdyacentes(arreglo3), 100000);
+        int[] arreglo3 = {2 ,2000000000};
+        System.out.println(p.productoAdyacentes(arreglo3));
         int[] arr2 = {1, -1, 1, -1, 1, -1};
         assertEquals(-1, p.productoAdyacentes(arr2));
         int[] arr1 = {1, -4, 2, 2, 5, -1};
         int[] a = {1, 2, 69, 8, 6};
         assertEquals(10, p.productoAdyacentes(arr1));
     }
+
 
 }
